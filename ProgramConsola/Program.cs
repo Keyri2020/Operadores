a37fc4ab03d8297a774f34f5d043e69a2f8a8872
Keyri2020 <70116371+Keyri2020@users.noreply.github.com>
2021-05-10 09:35:31 -0600

    Ejercicio 2

---
+++ ProgramConsola/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            int edad;
+            string sexo;
+            string respuesta;
+
+            do
+            {
+
+                Console.WriteLine("¿Cuál es su nombre?");
+                nombre = Console.ReadLine();
+
+                Console.WriteLine("");
+
+                Console.WriteLine("¿Qué edad tienes?");
+                edad = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("");
+
+                Console.WriteLine("¿Eres Masculino o Femenino? si es masculino escriba M y si es femenino, F");
+                sexo = Console.ReadLine();
+
+                if (sexo == "M" || sexo == "m")
+                {
+                    Console.WriteLine("");
+                    System.Console.WriteLine("El Sr. " + nombre + " ha sido registrado.");
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    System.Console.WriteLine("La Sra. " + nombre + " ha sido registrada.");
+                }
+
+                Console.WriteLine("\n");
+                Console.WriteLine("¿Desea ingresar nuevos datos?");
+                respuesta = Console.ReadLine();
+
+            } while (respuesta == "si" || respuesta == "Si");
+
+                Console.WriteLine("Programa finalizado");
+                Console.ReadKey();
+        }
+    }
+}
